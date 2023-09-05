2d37536f3c26162a7a4cdbff5f798f762a6481f4
[sirinmehmet] <[mhmtali45.sirin@gmail.com]>
2023-09-05 13:51:15 +0300

    05.09.2023

---
+++ MvcStok/Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using MvcStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcStok.Models
+{
+    public class Class1
+    {
+        public IEnumerable<TBLKATEGORILER> TBLKATEGORILER { get; set; }
+        public IEnumerable<TBLURUNLER> TBLURUNLER { get; set; }
+
+        public IEnumerable<TBLMUSTERILER> TBLMUSTERILER { get; set; }
+        public IEnumerable<TBLSATISLAR> TBLSATISLAR { get; set; }
+
+    }
+}
---
+++ MvcStok/Models/Entity/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MvcStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcStok.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<TBLKATEGORILER>KATEGORI{ get; set;}
+        public IEnumerable<TBLMUSTERILER>MUSTERI{ get; set;}
+        public IEnumerable<TBLSATISLAR>SATISLAR{ get; set; }
+        public IEnumerable <TBLURUNLER>URUNLER{ get; set; }     
+
+    }
+}
